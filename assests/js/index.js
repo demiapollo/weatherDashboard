c3942e27c45084cd500fd0486400e54aacea414e
demiapollo <apollodemirel67@gmail.com>
2023-04-04 11:19:21 -0700

    fixed weahter icons

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,5 +1,8 @@
 //making the api request
 const cityInputEl = document.getElementById("city-input");
+const containerEl = document.querySelector(".container");
+const subcontainerEl = document.querySelector(".subcontainer");
+const apiKey = "473b00c4ed0b74d622b98d1645a9c63d";
 //console.log(apiKey)
 
 function getApi() {
@@ -11,26 +14,38 @@ function getApi() {
     })
     .then(function (data) {
       console.log(data);
-      var htmlCard = "";
+      var htmlCard = " ";
       for (var i = 0; i < data.list.length; i = i + 8) {
         htmlCard += `
-            <div class="card bg-primary text-white p-2 m-2" style="width: 10rem;">
-            <h5 class="card-title">${data.list[i].weather[0].description}<span>
+            <div class="card bg-primary text-white p-1 m-1 fiveday" style="width: 11rem;">
+            <p class="card-text">${dayjs(data.list[i].dt_txt).format(
+              "MM/DD/YYYY"
+            )}</p>
+            
+            <span>
   <img src="https://openweathermap.org/img/wn/${
     data.list[i].weather[0].icon
-  }@2x.png"." class="card-img-top" height="70" width="10" alt="..."></span></h5>
-  <div class="card-body">
-    <h5 class="card-title">${cityInputEl.value}</h5>
+  }@2x.png"." class="card-img-top" alt="..."></span>
+  
+    
     <p class="card-text">Temprature ${data.list[i].main.temp}</p>
     <p class="card-text">Humidity ${data.list[i].main.humidity}</p>
     <p class="card-text">Wind Speed ${data.list[i].wind.speed}</p>
-    <p class="card-text">${dayjs(data.list[i].dt_txt).format("MM/DD/YYYY")}</p>
-  </div>
+    
+
 </div>
             `;
       }
       document.getElementById("forecastFiveDays").innerHTML = htmlCard;
+      const h5El = document.createElement("h5");
+      h5El.innerText = "5-Day Forecast: ";
+
+      const target = document.getElementById("forecastFiveDays");
+      target.parentNode.insertBefore(h5El, target);
     });
+
+  saveCity();
+  displayCity();
 }
 //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 function getCurrentDay() {
@@ -44,23 +59,49 @@ function getCurrentDay() {
       var htmlCard = "";
 
       htmlCard += `
-            <div class="card" style="width: 75%;">
+            <div class="card" style="width: 100%;">
+            <h5 class="card-title">${cityInputEl.value} <span>(${dayjs(
+        data.dt_txt
+      ).format("MM/DD/YYYY")})</span></h5>
             <h5 class="card-title">${data.weather[0].description}<span>
   <img src="https://openweathermap.org/img/wn/${
     data.weather[0].icon
   }@2x.png"." class="card-img-top" height="40" width="5" alt="..."></span></h5>
-  <div class="card-body">
-    <h5 class="card-title">${cityInputEl.value}</h5>
+
+    
     <p class="card-text">Temprature ${data.main.temp}</p>
     <p class="card-text">Humidity ${data.main.humidity}</p>
     <p class="card-text">Wind Speed ${data.wind.speed}</p>
-    <p class="card-text">${dayjs(data.dt_txt).format("MM/DD/YYYY")}</p>
-  </div>
+    
+
 </div>
             `;
 
       document.getElementById("currentWeather").innerHTML = htmlCard;
     });
+}
+
+//adding city input to local storage
+function saveCity() {
+  var city = document.getElementById("city-input").value;
+  localStorage.setItem("city", city);
+  city.innerHTML = `${city}`;
+  containerEl.prepend(city);
+
+  console.log(city);
+}
+
+//displaying the city from local storage
+function displayCity() {
+  for (let i = 0; i < localStorage.length; i++) {
+    var city = localStorage.getItem("city");
+    console.log(city);
+    var cityEl = document.createElement("button");
+    cityEl.textContent = city;
+    cityEl.classList.add("btn", "btn-primary", "btn-block", "mt-2");
+    cityEl.setAttribute("id", "city-button");
+    subcontainerEl.append(cityEl);
+  }
 }
 
 document.getElementById("search-button").addEventListener("click", getApi);
